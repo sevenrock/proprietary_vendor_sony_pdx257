91eb52e039738aea57096afa376914ec1fd8b221
Sevenrock <sevenrock@hotmail.de>
2026-08-28 19:16:41 +0200

    pdx257: Update WFD system blobs from marvel_g A171WEH.20
    Signed-off-by: Sevenrock <sevenrock@hotmail.de>

--- pdx257-vendor.mk
+++ pdx257-vendor.mk
@@ -1278,7 +1278,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.qccsyshal@1.2 \
     vendor.qti.hardware.qccvndhal@1.0 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
-    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
+    vendor.qti.hardware.wifidisplaysession_aidl-V2-ndk \
     vendor.qti.ims.datachannelservice-V2-ndk \
     vendor.qti.ims.datachannelservice-V3-ndk \
     vendor.qti.imsrtpservice@3.0 \
